0562c0dac1946829529f29f64c52bd3cdb2caae8
Francisco Albusac <francisco.albusac@bbc.co.uk>
2020-01-07 00:28:56 +0000

    more

--- src/main/kotlin/meeee/theater/control/ControllerQualifier.kt
+++ src/main/kotlin/meeee/theater/control/ControllerQualifier.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class ControllerWithQualifiers {
+class ControllerQualifier {
 
     @Autowired
     @Qualifier("happyFortuneService")
