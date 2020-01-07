c2b6777ba1ea4b97f698a44558701f9da968f39a
Francisco Albusac <francisco.albusac@bbc.co.uk>
2020-01-07 00:00:51 +0000

    added some more random stuff

--- src/main/kotlin/meeee/theater/control/MainController.kt
+++ src/main/kotlin/meeee/theater/control/MainController.kt
@@ -3,6 +3,7 @@ package meeee.theater.control
 import meeee.theater.service.BookingService
 import meeee.theater.service.Theater
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView
 @Controller
 class MainController {
 
+    // DI services
     @Autowired
     lateinit var theaterService: Theater
 
---
+++ src/main/kotlin/meeee/theater/control/MyRestController.kt
@@ -0,0 +1,34 @@
+package meeee.theater.control
+
+import meeee.theater.service.BookingService
+import meeee.theater.service.Theater
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
+
+@RestController
+class MyRestController {
+    // DI properties
+    @Value("\${coach.name}")
+    lateinit var coachName: String
+
+    @Value("\${team.name}")
+    lateinit var teamName: String
+
+    @GetMapping("/teamName")
+    fun teamName(): String
+    {
+        return teamName
+    }
+
+    @GetMapping("/coachName")
+    fun coachName(): String
+    {
+        return coachName
+    }
+}
